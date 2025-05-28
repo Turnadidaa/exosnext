a3cb22c621eb8a4cab9c7108f461ac63b3869b77
Turnadidaa <141144810+Turnadidaa@users.noreply.github.com>
2025-05-28 12:47:30 +0100

    2

--- WEEK4/day2/Dailychallenge/dc1.js
+++ WEEK4/day2/Dailychallenge/dc1.js
@@ -50,3 +50,42 @@
 // });
 
 // console.log(totalScore); // 71
+
+// Dailychallenge 2 
+
+// const inventory = [
+//   { id: 1, car_make: "Lincoln", car_model: "Navigator", car_year: 2009 },
+//   { id: 2, car_make: "Mazda", car_model: "Miata MX-5", car_year: 2001 },
+//   { id: 3, car_make: "Honda", car_model: "Accord", car_year: 1983 },
+//   { id: 4, car_make: "Land Rover", car_model: "Defender Ice Edition", car_year: 2010 },
+//   { id: 5, car_make: "Honda", car_model: "Accord", car_year: 1995 },
+// ];
+
+// // Part I
+// function getCarHonda(carInventory) {
+//   const firstHonda = carInventory.find(car => car.car_make === "Honda");
+//   if (firstHonda) {
+//     return `This is a ${firstHonda.car_make} ${firstHonda.car_model} from ${firstHonda.car_year}.`;
+//   } else {
+//     return "No Honda car found.";
+//   }
+// }
+
+// console.log(getCarHonda(inventory));
+// // Output: This is a Honda Accord from 1983.
+
+// // Part II
+// function sortCarInventoryByYear(carInventory) {
+//   return carInventory.slice().sort((a, b) => a.car_year - b.car_year);
+// }
+
+// console.log(sortCarInventoryByYear(inventory));
+// /*
+// [
+//   { id: 3, car_make: "Honda", car_model: "Accord", car_year: 1983 },
+//   { id: 5, car_make: "Honda", car_model: "Accord", car_year: 1995 },
+//   { id: 2, car_make: "Mazda", car_model: "Miata MX-5", car_year: 2001 },
+//   { id: 1, car_make: "Lincoln", car_model: "Navigator", car_year: 2009 },
+//   { id: 4, car_make: "Land Rover", car_model: "Defender Ice Edition", car_year: 2010 },
+// ]
+// */
